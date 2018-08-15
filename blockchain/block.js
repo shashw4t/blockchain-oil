bb4111af104605aec70cf5a5f6ae0ce164a55e42
shashw4t <41077597+shashw4t@users.noreply.github.com>
2018-08-15 11:41:07 -0400

    Block class with comments

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,22 +1,22 @@
 const ChainUtil = require('../chain-util');
 const { DIFFICULTY, MINE_RATE } = require('../config');
-//these variables from the config file are how hard the proof of work for mining a block and adding it onto the blockchain will be
+//These variables from the config file are how hard the proof of work for mining a block and adding it onto the blockchain will be
 
-
+//This class creates the block object that acts as an individual link in the chain.
 class Block {
   constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
     this.timestamp = timestamp; 
-    //when this block was produced, note this is affected by the time taken by mining
+    //When this block was produced, note this is affected by the time taken by mining
     this.lastHash = lastHash; 
-    //chains this block to the previous block in the chain, similar to a linked list
+    //Chains this block to the previous block in the chain, similar to a linked list
     this.hash = hash; 
     //SHA-256 hash produced by the values of the block run through the crypto-js hash function
     this.data = data; 
-    //this data is the transaction as this is a crptocurrency, but could be anything from anti-counterfeit measures to healthcare information
+    //This data is the transaction as this is a crptocurrency, but could be anything from anti-counterfeit measures to healthcare information
     this.nonce = nonce;
-    //this is a value made from counting each hash generation attempt and is used to quickly validate the block's hash
+    //This is a value made from counting each hash generation attempt and is used to quickly validate the block's hash
     this.difficulty = difficulty || DIFFICULTY;
-    //this dynamic difficulty level will oscillate to maintain the mining rate of adding blocks to the chain
+    //This dynamic difficulty level will oscillate to maintain the mining rate of adding blocks to the chain
   }
 
   toString() {
@@ -29,14 +29,14 @@ class Block {
       Data      : ${this.data}`;
   }
 
-  //this is the first block for the blockchain that needs to be created manually, so the future blocks can chain to this
-  //this doesn't store any data, and is mostly needed for it's hash value
+  //This is the first block for the blockchain that needs to be created manually, so the future blocks can chain to this
+  //This doesn't store any data, and is mostly needed for it's hash value
   static genesis() {
       return new this('Genesis time', '-----', 'theGenesisBlock', [], 0, DIFFICULTY);
   }
 
-  //this method allows users to mine a block of transactions and where the dynamic difficulty level is applied
-  //this blockchain’s default mine rate is 3000 milliseconds and the difficulty level is adjusted 
+  //This method allows users to mine a block of transactions and where the dynamic difficulty level is applied
+  //This blockchain’s default mine rate is 3000 milliseconds and the difficulty level is adjusted 
   //based on whether blocks are mined faster or slower
   
   static mineBlock(lastBlock, data) {
@@ -55,7 +55,7 @@ class Block {
       return new this(timestamp, lastHash, hash, data, nonce, difficulty);
   }
 
-  //this produces the hash value 
+  //This produces the hash value 
   static hash(timestamp, lastHash, data, nonce, difficulty) {
       return ChainUtil.hash(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
   }
@@ -65,7 +65,7 @@ class Block {
       return Block.hash(timestamp, lastHash, data, nonce, difficulty);
   }
 
-  //this method checks the timestamp from the previous block to see if the time passed is greater than or less than the mine rate
+  //This method checks the timestamp from the previous block to see if the time passed is greater than or less than the mine rate
   //and then accordingly decreases or increases the difficulty
   static adjustDifficulty(lastBlock, currentTime) {
       let { difficulty } = lastBlock; 
