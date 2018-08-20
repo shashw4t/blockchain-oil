b9b9e027b4aa5bba8382fec03e5443494d944606
shashw4t <41077597+shashw4t@users.noreply.github.com>
2018-08-20 11:54:25 -0400

    [DOC]

--- app/miner.js
+++ app/miner.js
@@ -9,7 +9,14 @@ class Miner {
         this.wallet = wallet;
         this.p2pServer = p2pServer;
     }
-
+    
+    // Mines a new transaction on the blockchain by:
+    //Validating transactions on the transaction pool
+    //Rewarding the miner
+    //Creating new block on the blockchain consisting of the newly validated transactions
+    //Synchronizes blockchains between all other peers
+    //Clears transaction pool
+    //Broadcasts to every miner to clear their transaction pool
     mine() {
         const validTransactions = this.transactionPool.validTransactions();
         validTransactions.push(
